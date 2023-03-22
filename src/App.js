d0b56f33d2f9c193ddae6c5c54480c2bf0f36122
TheSonOfBat34 <TheSonOfBat@gmail.com>
2023-03-22 12:46:47 +1100

    Added Key Hover Effect
    Had to move themeObj up a level to allow recognition of correct style on hover leave

--- src/App.js
+++ src/App.js
@@ -6,6 +6,32 @@ function App() {
   let [face, updateFace] = React.useState("");
   let [position, updatePosition] = React.useState("1");
 
+  let themeObj = {
+    "1":{
+        App: {backgroundColor: "#3b4664", color: 'white'},
+        themeSelectorInput: {backgroundColor: '#252c46'},
+        calculatorFace: {backgroundColor: '#181f32'},
+        calculatorKeypad: {backgroundColor: '#252d44'},
+        key: {backgroundColor: '#e5e4e0', color: '#5b5d66'}
+    },
+
+    "2":{
+        App: {backgroundColor: "#e6e6e6", color: '#3b3a34'},
+        themeSelectorInput: {backgroundColor: '#d4cdcd'},
+        calculatorFace: {backgroundColor: '#eeeeee'},
+        calculatorKeypad: {backgroundColor: '#d3cdcd'},
+        key: {backgroundColor: '#e5e4e0', color: '#5b5d66'}
+    },
+    
+    "3":{
+        App: {backgroundColor: "#17062a", color: '#ffe53b'},
+        themeSelectorInput: {backgroundColor: '#1e0837'},
+        calculatorFace: {backgroundColor: '#1e0836'},
+        calculatorKeypad: {backgroundColor: '#1e0836'},
+        key: {backgroundColor: '#331b4d', color: '#ffe53b'}
+    }
+}
+
   function keyPress(key){
     updateFace((prev)=>prev+key)
   }
@@ -14,8 +40,16 @@ function App() {
     updatePosition((prev)=>prev==="1"?"2":prev==="2"?"3":"1");
   }
 
+  function handleKeyEnter(key){
+    key.style.backgroundColor = "white";
+  }
+
+  function handleKeyLeave(key){
+    key.style.backgroundColor = themeObj[position]['key'].backgroundColor;
+  }
+
   return (
-    <Calculator face={face} onClick={(e)=>keyPress(e.target.innerText)} position={position} togglePosition={handleToggle}/>
+    <Calculator themeObj={themeObj} face={face} onClick={(e)=>keyPress(e.target.innerText)} position={position} togglePosition={handleToggle} hoverEnter={(e)=>handleKeyEnter(e.target)} hoverLeave={(e)=>handleKeyLeave(e.target)}/>
   );
 }
 
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,33 +1,8 @@
 export default function Calculator(props){
     let toggleStyle = props.position==="1"?{backgroundColor: '#e16a62', left: '0%', transform: 'translate(50%, -50%)'}:props.position==="2"?{backgroundColor:'#f88b3e', left: '50%', transform: 'translate(-50%, -50%)'}:{backgroundColor: '#92fff9', left: '100%', transform: 'translate(-150%, -50%)'};
-    let themeObj = {
-        "1":{
-            App: {backgroundColor: "#3b4664", color: 'white'},
-            themeSelectorInput: {backgroundColor: '#252c46'},
-            calculatorFace: {backgroundColor: '#181f32'},
-            calculatorKeypad: {backgroundColor: '#252d44'},
-            key: {backgroundColor: '#e5e4e0', color: '#5b5d66'}
-        },
-
-        "2":{
-            App: {backgroundColor: "#e6e6e6", color: '#3b3a34'},
-            themeSelectorInput: {backgroundColor: '#d4cdcd'},
-            calculatorFace: {backgroundColor: '#eeeeee'},
-            calculatorKeypad: {backgroundColor: '#d3cdcd'},
-            key: {backgroundColor: '#e5e4e0', color: '#5b5d66'}
-        },
         
-        "3":{
-            App: {backgroundColor: "#17062a", color: '#ffe53b'},
-            themeSelectorInput: {backgroundColor: '#1e0837'},
-            calculatorFace: {backgroundColor: '#1e0836'},
-            calculatorKeypad: {backgroundColor: '#1e0836'},
-            key: {backgroundColor: '#331b4d', color: '#ffe53b'}
-        }
-    }
-    
     return(
-        <div className="App" style={themeObj[props.position]['App']}>
+        <div className="App" style={props.themeObj[props.position]['App']}>
             <div className="calculator--container">
                         <div className="calculator--top">
                             <div className="top--logo">Ryan's Calc</div>
@@ -39,31 +14,31 @@ export default function Calculator(props){
                                         <span>2</span>
                                         <span>3</span>
                                     </div>
-                                    <div className="themeSelector--input" style={themeObj[props.position]['themeSelectorInput']}>
+                                    <div className="themeSelector--input" style={props.themeObj[props.position]['themeSelectorInput']}>
                                         <div className="themeSelector--ball" id="ball" style={toggleStyle} onClick={props.togglePosition} ></div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="calculator--main">
-                            <div className="calculator--face" id="face"  style={themeObj[props.position]['calculatorFace']}>{props.face}</div>
-                            <div className="calculator--keypad" style={themeObj[props.position]['calculatorKeypad']}>
-                                <div style={themeObj[props.position]['key']} onClick={props.onClick} className="key">7</div>
-                                <div style={themeObj[props.position]['key']} onClick={props.onClick} className="key">8</div>
-                                <div style={themeObj[props.position]['key']} onClick={props.onClick} className="key">9</div>
-                                <div style={themeObj[props.position]['key']} onClick={props.onClick} className="key del">DEL</div>
-                                <div style={themeObj[props.position]['key']} onClick={props.onClick} className="key">4</div>
-                                <div style={themeObj[props.position]['key']} onClick={props.onClick} className="key">5</div>
-                                <div style={themeObj[props.position]['key']} onClick={props.onClick} className="key">6</div>
-                                <div style={themeObj[props.position]['key']} onClick={props.onClick} className="key">+</div>
-                                <div style={themeObj[props.position]['key']} onClick={props.onClick} className="key">1</div>
-                                <div style={themeObj[props.position]['key']} onClick={props.onClick} className="key">2</div>
-                                <div style={themeObj[props.position]['key']} onClick={props.onClick} className="key">3</div>
-                                <div style={themeObj[props.position]['key']} onClick={props.onClick} className="key">-</div>
-                                <div style={themeObj[props.position]['key']} onClick={props.onClick} className="key">.</div>
-                                <div style={themeObj[props.position]['key']} onClick={props.onClick} className="key">0</div>
-                                <div style={themeObj[props.position]['key']} onClick={props.onClick} className="key">/</div>
-                                <div style={themeObj[props.position]['key']} onClick={props.onClick} className="key">x</div>
+                            <div className="calculator--face" id="face"  style={props.themeObj[props.position]['calculatorFace']}>{props.face}</div>
+                            <div className="calculator--keypad" style={props.themeObj[props.position]['calculatorKeypad']}>
+                                <div onMouseEnter={props.hoverEnter} onMouseLeave={props.hoverLeave} style={props.themeObj[props.position]['key']} onClick={props.onClick} className="key">7</div>
+                                <div onMouseEnter={props.hoverEnter} onMouseLeave={props.hoverLeave} style={props.themeObj[props.position]['key']} onClick={props.onClick} className="key">8</div>
+                                <div onMouseEnter={props.hoverEnter} onMouseLeave={props.hoverLeave} style={props.themeObj[props.position]['key']} onClick={props.onClick} className="key">9</div>
+                                <div onMouseEnter={props.hoverEnter} onMouseLeave={props.hoverLeave} style={props.themeObj[props.position]['key']} onClick={props.onClick} className="key">DEL</div>
+                                <div onMouseEnter={props.hoverEnter} onMouseLeave={props.hoverLeave} style={props.themeObj[props.position]['key']} onClick={props.onClick} className="key">4</div>
+                                <div onMouseEnter={props.hoverEnter} onMouseLeave={props.hoverLeave} style={props.themeObj[props.position]['key']} onClick={props.onClick} className="key">5</div>
+                                <div onMouseEnter={props.hoverEnter} onMouseLeave={props.hoverLeave} style={props.themeObj[props.position]['key']} onClick={props.onClick} className="key">6</div>
+                                <div onMouseEnter={props.hoverEnter} onMouseLeave={props.hoverLeave} style={props.themeObj[props.position]['key']} onClick={props.onClick} className="key">+</div>
+                                <div onMouseEnter={props.hoverEnter} onMouseLeave={props.hoverLeave} style={props.themeObj[props.position]['key']} onClick={props.onClick} className="key">1</div>
+                                <div onMouseEnter={props.hoverEnter} onMouseLeave={props.hoverLeave} style={props.themeObj[props.position]['key']} onClick={props.onClick} className="key">2</div>
+                                <div onMouseEnter={props.hoverEnter} onMouseLeave={props.hoverLeave} style={props.themeObj[props.position]['key']} onClick={props.onClick} className="key">3</div>
+                                <div onMouseEnter={props.hoverEnter} onMouseLeave={props.hoverLeave} style={props.themeObj[props.position]['key']} onClick={props.onClick} className="key">-</div>
+                                <div onMouseEnter={props.hoverEnter} onMouseLeave={props.hoverLeave} style={props.themeObj[props.position]['key']} onClick={props.onClick} className="key">.</div>
+                                <div onMouseEnter={props.hoverEnter} onMouseLeave={props.hoverLeave} style={props.themeObj[props.position]['key']} onClick={props.onClick} className="key">0</div>
+                                <div onMouseEnter={props.hoverEnter} onMouseLeave={props.hoverLeave} style={props.themeObj[props.position]['key']} onClick={props.onClick} className="key">/</div>
+                                <div onMouseEnter={props.hoverEnter} onMouseLeave={props.hoverLeave} style={props.themeObj[props.position]['key']} onClick={props.onClick} className="key">x</div>
                             </div>
                             <div className="key">RESET</div>
                             <div className="key">=</div>
