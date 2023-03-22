2385a83fa78e89c140ddfb06335052a8dffd0a76
TheSonOfBat34 <TheSonOfBat@gmail.com>
2023-03-22 12:32:25 +1100

    Overhauled into React
    New facilitates change of theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Calculator from './Calculator';
 
 function App() {
+  let [face, updateFace] = React.useState("");
+  let [position, updatePosition] = React.useState("1");
+
+  function keyPress(key){
+    updateFace((prev)=>prev+key)
+  }
+
+  function handleToggle(){
+    updatePosition((prev)=>prev==="1"?"2":prev==="2"?"3":"1");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Calculator face={face} onClick={(e)=>keyPress(e.target.innerText)} position={position} togglePosition={handleToggle}/>
   );
 }
 
